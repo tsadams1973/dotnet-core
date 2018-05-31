d4d1533b388c5227188761e8641b381e2232104d
Timothy Adams <adamst@epi.usf.edu>
2018-05-31 11:57:59 -0400

    Initial commit identity template project'

---
+++ IdentityAuthorization/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityAuthorization.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
