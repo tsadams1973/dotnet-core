525a8ebaf28ec4ca617f83a6ebb14257cd4de15d
Timothy Adams <adamst@epi.usf.edu>
2018-06-07 11:41:32 -0400

    setting up structure of command

--- SignAndAlign/Program.cs
+++ SignAndAlign/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace SignAndAlign
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {            
+            var app = new CommandLineApplication();
+
+            var apk = app.Command("apk",config => {
+                config.Description = "Sign and align the apk.";
+                config.HelpOption("--help");
+                var apkname= config.Argument("apkname", "The name of the apk.",false);
+                var keystorename = config.Argument("keystore","The name of your keystore.",false);
+                var keypass = config.Argument("password","The password for your keystore.",false);
+                config.OnExecute(()=>{
+
+                    
+
+                    Console.WriteLine($"Signed {apkname.Value} using {keystorename.Value} and password {keypass.Value}.");
+                    return 0;
+
+
+                });
+            });
+            
+            apk.Command("align", config=>{
+                config.OnExecute(()=>{
+                    Console.WriteLine("Only align the apk.");
+                    return 1; //not implemented
+                });
+            });
+
+            apk.Command("sign", config=>{
+                config.OnExecute(()=>{
+                    Console.WriteLine("Only sign the apk.");
+                    return 1; //not implemented
+                });
+            });            
+
+            app.HelpOption("--help");
+
+            var result = app.Execute(args);
+            Environment.Exit(result);
         }
     }
 }
---
+++ SignAndAlign/readme.md
@@ -0,0 +1,6 @@
+## Quick CLI
+While building and pushing an Ionic app to the play console, I thought I would consolidate the CLI commands for aligning and signing the apks. This assumes you already built the app and have a keystore.
+
+##CLI Reference
+To create the app: `dotnet new console`
+To add the CLI package: `dotnet add package Microsoft.Extensions.CommandLineUtils`
