9db88492080951475591d891ec4ea5b9c8e732fe
Timothy Adams <adamst@epi.usf.edu>
2018-06-07 08:28:50 -0400

    Added new console project

---
+++ SignAndAlign/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SignAndAlign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
